b0260b1cf7f6708570f08660a0fe1ac767f73352
Damian Kaczorowski <kaczorowskid97@gmail.com>
2024-12-06 13:27:29 +0100

    feat: add radio class

--- main-board/src/main.cpp
+++ main-board/src/main.cpp
@@ -2,15 +2,19 @@
 #include <ArduinoJson.h>
 #include "esp32.h"
 #include "date.h"
+#include "radio.h"
+#include "../secrets.h"
 
 Esp32 esp32;
 Date date;
+Radio radio;
 
 void setup()
 {
   esp32.initSerial(9600);
   esp32.initWifi();
   date.initDate();
+  radio.initRadio(RF24_ADDR);
 }
 
 void loop()
